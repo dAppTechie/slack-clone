eaead71882bc860eb2789ea0e62c334571673c3c
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2022-01-23 12:11:44 -0700

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAKe-GLTPh3BU_uVfFcLAWnYEEJdEfkT4w',
+  authDomain: 'slack-clone-1360f.firebaseapp.com',
+  projectId: 'slack-clone-1360f',
+  storageBucket: 'slack-clone-1360f.appspot.com',
+  messagingSenderId: '908123936070',
+  appId: '1:908123936070:web:f795cf662a3b1c5e18c147',
+};
