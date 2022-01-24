0c10f7b652be6980308655a930bfaa59c21f10d4
Chad Parker <63312621+imchad-me@users.noreply.github.com>
2022-01-23 18:53:42 -0700

    Started Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { db } from '../firebase';
 
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
+import InserCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 import styled from 'styled-components';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
+  const [channels, loading, error] = useCollection(db.collection('rooms'));
   return (
     <SidebarContainer>
       <SidebarHeader>
@@ -18,6 +32,26 @@ function Sidebar() {
         </SidebarInfo>
         <CreateIcon />
       </SidebarHeader>
+      <SidebarOption Icon={InserCommentIcon} title='Threads' />
+      <SidebarOption Icon={InboxIcon} title='Mentions & reactions' />
+      <SidebarOption Icon={DraftsIcon} title='Saved items' />
+      <SidebarOption Icon={BookmarkBorderIcon} title='Channel browser' />
+      <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+      <SidebarOption Icon={AppsIcon} title='Apps' />
+      <SidebarOption Icon={FileCopyIcon} title='File browser' />
+      <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption title='Add Channel' />
+      {channels?.docs.map((doc) => (
+        <SidebarOption
+          key={doc.id}
+          id={doc.id}
+          addChannelOption
+          title={doc.data().name}
+        />
+      ))}
     </SidebarContainer>
   );
 }
@@ -31,6 +65,12 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 250px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
@@ -47,4 +87,26 @@ const SidebarHeader = styled.div`
   }
 `;
 
-const SidebarInfo = styled.div``;
+const SidebarInfo = styled.div`
+  flex: 1;
+
+  > h2 {
+    font-size: 15px;
+    font-weight: 900;
+    margin-bottom: 5px;
+  }
+
+  > h3 {
+    display: flex;
+    font-size: 13px;
+    font-weight: 400;
+    align-items: center;
+  }
+
+  > h3 > .MuiSvgIcon-root {
+    font-size: 14px;
+    margin-top: 1px;
+    margin-right: 2px;
+    color: green;
+  }
+`;
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyAKe-GLTPh3BU_uVfFcLAWnYEEJdEfkT4w',
   authDomain: 'slack-clone-1360f.firebaseapp.com',
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: '908123936070',
   appId: '1:908123936070:web:f795cf662a3b1c5e18c147',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
